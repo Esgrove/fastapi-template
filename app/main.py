baa5d7d7c84fb1975d6d6a63db3a7d58d84cbfc7
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-04-26 12:24:40 +0300

    add a few example routes

--- app/main.py
+++ app/main.py
@@ -1,14 +1,31 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from mangum import Mangum
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("favicon.ico")
+
+
 @app.get("/")
-async def main_route():
+async def root_route():
     return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/items/")
+async def read_items(skip: int = 0, limit: int = 10):
+    fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+    return fake_items_db[skip : skip + limit]
 
 
 def start():

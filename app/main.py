2c8f462e243ef45cff79b25f3f9e2dc6e81d538f
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-06-14 16:23:52 +0300

    add pydantic usage to specify types

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+import random
+
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
@@ -7,30 +9,49 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+class Message(BaseModel):
+    message: str
+
+
+class Item(BaseModel):
+    name: str
+    item_id: int = random.randint(1000, 9999)
+
+
 @app.get("/favicon.ico", include_in_schema=False)
-async def favicon():
+async def favicon() -> FileResponse:
     return FileResponse("favicon.ico")
 
 
 @app.get("/")
-async def root_route():
-    return {"message": "Hello World"}
+async def root_route() -> Message:
+    """Show hello message."""
+    return Message(message="Hello World")
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(item_id: int) -> Item:
+    """Return item for given id."""
+    return Item(name="test", item_id=item_id)
 
 
 @app.get("/items/")
-async def read_items(skip: int = 0, limit: int = 10):
-    fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+async def read_items(skip: int = 0, limit: int = 3) -> list[Item]:
+    """
+    Get a list of items.
+
+    optionally starting from `skip` index.
+    Optionally up to `limit` items.
+    """
+    fake_items_db = [Item(name=name) for name in ("foo", "bar", "baz", "lorem", "ipsum", "dolor", "sit", "amet")]
     return fake_items_db[skip : skip + limit]
 
 
-def start():
+def start() -> None:
     """Helper for starting server with `poetry run start`."""
     uvicorn.run("app.main:app", reload=True)
 
 
+# Mangum is an adapter for running ASGI applications in AWS Lambda
+# https://mangum.io/
 handler = Mangum(app, lifespan="off")
--- tests/test_main.py
+++ tests/test_main.py
@@ -9,3 +9,15 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+
+def test_get_item():
+    for item_id in (1, 1000, 354):
+        response = client.get(f"/items/{item_id}")
+        assert response.status_code == 200
+        assert response.json() == {"name": "test", "item_id": item_id}
+
+
+def test_read_items():
+    response = client.get(f"/items/")
+    assert response.status_code == 200

711903bb4551c796a5571fb23eb6cec3c7b34518
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-10-25 18:05:35 +0300

    wait for api to start in run script

--- app/version.py
+++ app/version.py
@@ -4,6 +4,6 @@ Akseli Lukkarila
 2019-2023
 """
 BRANCH = "main"
-COMMIT = "9f78af3"
-DATE = "2023-10-25_1747"
+COMMIT = "2dd997b"
+DATE = "2023-10-25_1804"
 VERSION_NUMBER = "1.0.0"
--- run.sh
+++ run.sh
@@ -17,16 +17,29 @@ if [ -z "$(docker ps -q --filter "name=fastapi")" ]; then
 
     print_magenta "Running API..."
     docker run -d --name fastapi -p 80:80 runtime
+
+    echo "Waiting for container to start..."
+    while ! curl -f http://127.0.0.1 > /dev/null; do
+        sleep 1
+    done
+    print_green "API is running"
 else
     echo "Docker image is running, skipping build..."
 fi
 
+print_magenta "Testing routes..."
 echo "http://127.0.0.1"
 curl -s http://127.0.0.1 | jq .
+
+echo "http://127.0.0.1/version/"
+curl -s http://127.0.0.1/version/ | jq .
+
 echo "http://127.0.0.1/items/123"
 curl -s http://127.0.0.1/items/1234 | jq .
+
 echo "http://127.0.0.1/items/"
 curl -s http://127.0.0.1/items/ | jq .
+
 echo "http://127.0.0.1/items/?limit=8"
 curl -s http://127.0.0.1/items/?limit=8 | jq .
 

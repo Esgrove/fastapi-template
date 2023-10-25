260286c77891cb1decfa496706ae408ff59602be
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2023-10-25 16:26:13 +0300

    update common shell functions

--- common.sh
+++ common.sh
@@ -2,6 +2,7 @@
 set -eo pipefail
 
 REPO_ROOT=$(git rev-parse --show-toplevel || (cd "$(dirname "${BASH_SOURCE[0]}")" && pwd))
+export REPO_ROOT
 
 # Print a message with green color
 print_green() {
@@ -23,9 +24,17 @@ print_yellow() {
     printf "\e[1;49;33m%s\e[0m\n" "$1"
 }
 
+print_error() {
+    print_red "ERROR: $1"
+}
+
+print_warn() {
+    print_yellow "WARNING: $1"
+}
+
 # Print an error message and exit the program
 print_error_and_exit() {
-    print_red "ERROR: $1"
+    print_error "$1"
     # use exit code if given as argument, otherwise default to 1
     exit "${2:-1}"
 }

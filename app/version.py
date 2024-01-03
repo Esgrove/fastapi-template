25239f932ceb1f52966f549d90e1705d88a952e3
Akseli Lukkarila <akseli.lukkarila@nitor.com>
2024-01-03 14:45:21 +0200

    update version info

--- app/version.py
+++ app/version.py
@@ -3,7 +3,9 @@ Version information definitions
 Akseli Lukkarila
 2019-2023
 """
+
+# CREATED BY SCRIPT. DO NOT MODIFY MANUALLY.
 BRANCH = "main"
-COMMIT = "ad6b7b2"
-DATE = "2023-10-25_1809"
+COMMIT = "385fd79"
+DATE = "2024-01-03_1445"
 VERSION_NUMBER = "1.0.0"
